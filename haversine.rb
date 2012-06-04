6c0bb8679624e0abe0d4cb80714e85a6ddcbbe9b
Hwan-Joon Choi <hc5duke@gmail.com>
2012-06-03 20:57:38 -0700

    ruby-fy

--- haversine.rb
+++ haversine.rb
@@ -8,43 +8,38 @@
 # Descrption: Example script implementing Haversine formula to identify distance in KM between 2 coordinates
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 #
+# Ruby-fied by @hc5duke 2012-06-03
 
-def coorDist(lat1, lon1, lat2, lon2)
 
-    earthRadius = 6371 # Earth's radius in KM
+def coor_dist(lat1, lon1, lat2, lon2)
+  def to_radian(value)
+    value * Math::PI / 180.0
+  end
+  earth_radius = 6371 # Earth's radius in KM
 
-        # convert degrees to radians
-        def convDegRad(value)
-          unless value.nil? or value == 0
-                value = (value/180) * Math::PI
-          end
-        return value
-        end
+  lat1_radian = to_radian(lat1)
+  lat2_radian = to_radian(lat2)
+  delta_lat_radian = to_radian(lat2 - lat1)
+  delta_lon_radian = to_radian(lon2 - lon1)
 
-    deltaLat = (lat2-lat1)
-    deltaLon = (lon2-lon1)
-    deltaLat = convDegRad(deltaLat)
-    deltaLon = convDegRad(deltaLon)
+  # Calculate square of half the chord length between latitude and longitude
+  a = Math.sin(delta_lat_radian / 2) * Math.sin(delta_lat_radian / 2) +
+      Math.cos(lat1_radian) * Math.cos(lat2_radian) *
+      Math.sin(delta_lon_radian / 2) * Math.sin(delta_lon_radian / 2)
 
-    # Calculate square of half the chord length between latitude and longitude
-    a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
-        Math.cos((lat1/180 * Math::PI)) * Math.cos((lat2/180 * Math::PI)) *
-        Math.sin(deltaLon/2) * Math.sin(deltaLon/2); 
+  # Calculate the angular distance in radians
+  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-    # Calculate the angular distance in radians
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-
-    distance = earthRadius * c
-
-  return distance
-
+  earth_radius * c
 end
 
-    # hardcoded example coordinates #
-    lat1 = 41.5592880250001
-    lat2 = 41.8574079970001
-    lon1 = -73.0371704099999
-    lon2 = -72.00779572399991
-    #################################
 
-    puts coorDist(lat1, lon1, lat2, lon2)
+
+# hardcoded example coordinates #
+lat1 = 41.5592880250001
+lat2 = 41.8574079970001
+lon1 = -73.0371704099999
+lon2 = -72.00779572399991
+#################################
+
+puts coor_dist(lat1, lon1, lat2, lon2)
